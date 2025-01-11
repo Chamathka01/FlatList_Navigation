cc5c31ab15fb05818a8b4de51db91804cb875faf
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 16:41:00 +0000

    Button for add students

--- FLATLISTAPP/components/AddStudent.js
+++ FLATLISTAPP/components/AddStudent.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { Image } from "react-native";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 export default function AddStudent(){
     const [name,setName]=useState();
     return(
         <View>
-           <TextInput mode="outlined" label="Name" value=""/>
+           <TextInput mode="outlined" label="Name" value={name} onChangeText={setName}/>
+            <Button>ADD</Button>
         </View>
     );
 }
