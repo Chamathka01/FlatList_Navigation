a21bb7883c7b14dc1a4472662fa294f250ba8971
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 16:54:16 +0000

    Added new student

--- FLATLISTAPP/components/AddStudent.js
+++ FLATLISTAPP/components/AddStudent.js
@@ -4,12 +4,18 @@ import { View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 export default function AddStudent(){
     const [name,setName]=useState();
+    const newStudent={
+        name:name,
+        id:"451a",
+        age:15
+
+}
     return(
         <View>
            <TextInput mode="outlined" label="Name" value={name} onChangeText={setName}/>
             <Button onPress={()=>{
                 //Pas params back to home screen
-                navigation.popTo('StudentList',{post:postText});
+                navigation.popTo('StudentList',{newStudent:postText});
             }}>ADD</Button>
         </View>
     );
