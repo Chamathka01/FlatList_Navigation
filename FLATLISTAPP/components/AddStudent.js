529bed3d1d683d7d1f7265a0a9ac6e48f1a5b69f
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 16:37:17 +0000

    Added TextInput for component

--- FLATLISTAPP/components/AddStudent.js
+++ FLATLISTAPP/components/AddStudent.js
@@ -4,11 +4,7 @@ import { Text } from "react-native-paper";
 export default function AddStudent(){
     return(
         <View>
-           <Text>Name:{student.name}</Text> 
-           <Text>Age:{student.age}</Text> 
-           <Text>Gender:{student.gender}</Text> 
-           <Text>Address:{student.adddress}</Text>
-           <Image source={student.profile_pic}/> 
+           <TextInput mode="outlined" label="Name" value=""/>
         </View>
     );
 }
