e419a069473168effdaee9ed507b5c863f4ae1a8
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 15:45:59 +0000

    Used navigation

--- FLATLISTAPP/App.js
+++ FLATLISTAPP/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StudentList from './components/StudentList';
-
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
-   
+    <NavigationContainer>
       <StudentList />
+    </NavigationContainer>
+
     
   );
 }
--- FLATLISTAPP/components/Profile.js
+++ FLATLISTAPP/components/Profile.js
@@ -1,7 +1,12 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
 export default function Profile(){
     return(
         <View>
-            
+           <Text>Name:</Text> 
+           <Text>Age:</Text> 
+           <Text>Gender:</Text> 
+           <Text>Address:</Text> 
         </View>
     );
 }
