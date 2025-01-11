315918676c90a2d4a19c2065a6091647a539678e
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 15:48:49 +0000

    Create a stack navigator

--- FLATLISTAPP/App.js
+++ FLATLISTAPP/App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StudentList from './components/StudentList';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 export default function App() {
+  const Stack = createStackNavigator();
   return (
+    <SafeAreaProvider>
     <NavigationContainer>
       <StudentList />
     </NavigationContainer>
+    </SafeAreaProvider>
 
     
   );
