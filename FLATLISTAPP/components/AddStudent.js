0c49d7c57b80fbdaa99bea303fbd70a39f3a837c
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 16:38:32 +0000

    Add useState

--- FLATLISTAPP/components/AddStudent.js
+++ FLATLISTAPP/components/AddStudent.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Image } from "react-native";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 export default function AddStudent(){
+    const [name,setName]=useState();
     return(
         <View>
            <TextInput mode="outlined" label="Name" value=""/>
