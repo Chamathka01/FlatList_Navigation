f69213677336d7d5f75eb0d24e1fb297ddfab534
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-11 16:26:37 +0000

    Use Images for profile

--- FLATLISTAPP/components/Profile.js
+++ FLATLISTAPP/components/Profile.js
@@ -1,12 +1,14 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 export default function Profile({route}){
+    const {student} =route.params;
     return(
         <View>
-           <Text>Name:</Text> 
-           <Text>Age:</Text> 
-           <Text>Gender:</Text> 
-           <Text>Address:</Text> 
+           <Text>Name:{student.name}</Text> 
+           <Text>Age:{student.age}</Text> 
+           <Text>Gender:{student.gender}</Text> 
+           <Text>Address:{student.adddress}</Text>
+           <Image source={student.profile_pic}/> 
         </View>
     );
 }
