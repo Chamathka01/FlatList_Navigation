94b20f39c41a41ac70b3f48e3da8e049c15ab5d1
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-12 14:03:18 +0000

    Renders the screen

--- FLATLISTAPP/components/Profile.js
+++ FLATLISTAPP/components/Profile.js
@@ -7,7 +7,8 @@ export default function Profile({route}){
         <View>
            <Text>Name:{student.name}</Text> 
            <Text>Age:{student.age}</Text> 
-           <Text>Gender:{student.gender}</Text> 
+           <Text>Phone:{student.phone}</Text>
+           <Text>Email:{student.email}</Text>  
            <Text>Address:{student.adddress}</Text>
            <Image source={student.profile_pic}/> 
         </View>
--- FLATLISTAPP/components/UpdateStudent.js
+++ FLATLISTAPP/components/UpdateStudent.js
@@ -3,7 +3,41 @@ export default function UpdateStudent({route}){
     return(
         <View>
             <Text>Update Student</Text>
-            
+            <Text>Name:</Text>
+            <TextInput
+                style={styles.input}
+                mode="outlined"
+                value={name}
+                onChangeText={setName}
+            />
+            <Text>Age:</Text>
+            <TextInput
+                style={styles.input}
+                mode="outlined"
+                value={age}
+                onChangeText={setAge}
+            />
+            <Text>Phone:</Text>
+            <TextInput
+                style={styles.input}
+                mode="outlined"
+                value={phone}
+                onChangeText={setPhone}
+            />
+            <Text>Email:</Text>
+            <TextInput
+                style={styles.input}
+                mode="outlined"
+                value={email}
+                onChangeText={setEmail}
+            />
+            <Text>Address:</Text>
+            <TextInput
+                style={styles.input}
+                mode="outlined"
+                value={address}
+                onChangeText={setAddress}
+            />
         </View>
 
 
