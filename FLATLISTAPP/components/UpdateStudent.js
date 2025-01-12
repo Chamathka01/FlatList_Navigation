2f395308515a4391befc1613d5a9a0355b08b373
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-12 13:57:09 +0000

    Added title for update student

---
+++ FLATLISTAPP/components/UpdateStudent.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+export default function UpdateStudent({route}){
+    return(
+        <View>
+            <Text>Update Student</Text>
+            
+        </View>
+
+
+    );
+}
